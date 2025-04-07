5435cbd2e163ed35a80d93633892903b1c0f1b81
Johnny Kava <johnnylk2003@hotmail.com>
2025-04-07 14:05:49 -0300

    Add classe ninja basico, mais atributos

---
+++ src/NinjaBasico.java
@@ -0,0 +1,15 @@
+public class NinjaBasico implements Ninja{
+    String nome;
+    int idade;
+    String habilidade;
+
+    @Override
+    public void mostrarInformacoes() {
+
+    }
+
+    @Override
+    public void excutarHabilidade() {
+
+    }
+}
