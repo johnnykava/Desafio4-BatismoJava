d00bbdb614765a32973023ff805a14a2dea5a409
Johnny Kava <johnnylk2003@hotmail.com>
2025-04-08 11:22:56 -0300

    Criando os objetos e testando

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
+        NinjaBasico ninjaBasico = new NinjaBasico("Iruka", 40, "Multiplas Shurikens");
 
+        ninjaBasico.mostrarInformacoes();
+        ninjaBasico.excutarHabilidade();
+
+        NinjaAvancado ninjaAvancado = new NinjaAvancado("Sasuke Uchiha", 18, "Chidory", "Sharingan");
+
+        ninjaAvancado.mostrarInformacoes();
+        ninjaAvancado.excutarHabilidade();
+        ninjaAvancado.executarHabilidadeEspecial();
     }
 }
