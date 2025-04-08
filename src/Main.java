bb0ae9159c885e6030d4f33e81a8210fc34323fa
Johnny Kava <johnnylk2003@hotmail.com>
2025-04-08 11:40:11 -0300

    mudando habilidades para usar enum

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        NinjaBasico ninjaBasico = new NinjaBasico("Iruka", 40, "Multiplas Shurikens");
+        NinjaBasico ninjaBasico = new NinjaBasico("Iruka", 40, TipoHabilidade.NINJUTSU);
 
         ninjaBasico.mostrarInformacoes();
         ninjaBasico.excutarHabilidade();
 
-        NinjaAvancado ninjaAvancado = new NinjaAvancado("Sasuke Uchiha", 18, "Chidory", "Sharingan");
+        NinjaAvancado ninjaAvancado = new NinjaAvancado("Sasuke Uchiha", 18, TipoHabilidade.KATON, TipoHabilidade.RINNEGAN);
 
         ninjaAvancado.mostrarInformacoes();
         ninjaAvancado.excutarHabilidade();
